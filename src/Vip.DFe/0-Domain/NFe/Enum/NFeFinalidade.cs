181f4b10a088957292e39d6b56d4cce24b66b7f0
Leandro Ferreira <leandro@vipsolucoes.com>
2020-12-14 11:43:13 -0300

    Corrige descrições em NFeFinalidade

--- src/Vip.DFe/0-Domain/NFe/Enum/NFeFinalidade.cs
+++ src/Vip.DFe/0-Domain/NFe/Enum/NFeFinalidade.cs
@@ -8,13 +8,13 @@ namespace Vip.DFe.NFe.Enum
         [DFeEnum("1")] [Description("1 - NFe Normal")]
         Normal = 1,
 
-        [DFeEnum("2")] [Description("1 - NFe Complementar")]
+        [DFeEnum("2")] [Description("2 - NFe Complementar")]
         Complementar = 2,
 
-        [DFeEnum("3")] [Description("1 - NFe de Ajuste")]
+        [DFeEnum("3")] [Description("3 - NFe de Ajuste")]
         Ajuste = 3,
 
-        [DFeEnum("4")] [Description("1 - Devolução de Mercadoria")]
+        [DFeEnum("4")] [Description("4 - Devolução de Mercadoria")]
         Devolucao = 4
     }
 }
